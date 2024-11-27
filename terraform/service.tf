a449f3f88b7aeb2eb52c017cc8a9b47966e8a5ec
Shanuka Lakshan <89212510+ShanukaLakshan@users.noreply.github.com>
2024-11-27 15:38:42 +0530

    Add Docker image resource and project ID variable for user microservice in Terraform

--- terraform/service.tf
+++ terraform/service.tf
@@ -1,6 +1,13 @@
+resource "docker_image" "user_microservice_tf_docker_image" {
+  #   name = "asia-south1-docker.pkg.dev/steady-course-442905-f1/user-microservice-tf-repository/user-microservice-tf-docker-image"
+  name = "asia-south1-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.user_microservice_tf_registry.name}/user-microservice-tf:latest"
+  build {
+    path = "../"
+  }
+}
 
-resource "google_artifact_registry_repository" "user_microservice_tf_registry_repository" {
-  repository_id = "user-microservice-tf-repository"
+resource "google_artifact_registry_repository" "user_microservice_tf_registry" {
+  repository_id = "user-microservice-tf-registry"
   location      = "asia-south1"
   format        = "DOCKER"
 }
@@ -8,12 +15,5 @@ resource "google_artifact_registry_repository" "user_microservice_tf_registry_re
 resource "docker_registry_image" "user_microservice_tf_registry_image" {
   name          = docker_image.user_microservice_tf_docker_image.name
   keep_remotely = true
-}
-
-resource "docker_image" "user_microservice_tf_docker_image" {
-  name = "asia-south1-docker.pkg.dev/steady-course-442905-f1/user-microservice-tf-repository/user-microservice-tf-docker-image"
-  build {
-    path = "../"
-    tag  = ["user-microservice-tf:latest"]
-  }
+  depends_on    = [docker_image.user_microservice_tf_docker_image, google_artifact_registry_repository.user_microservice_tf_registry]
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "project_id" {
+  description = "The ID of the project in which to create resources."
+  type        = string
+  default     = "steady-course-442905-f1"
+}
